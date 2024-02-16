80f73a187c77f34acc6ad244e5caf693ed50f78a
jlamancio <amancio.jluis@outlook.com>
2024-02-16 18:01:44 -0300

    aula238 -  Generics - [Implementação básica com lista do tipo OBJECT - sem o uso do Generics - problemas: Type Safety]

--- src/main/java/cursoProgramacaoA236_A249GenericsSetMap/services/PrintService.java
+++ src/main/java/cursoProgramacaoA236_A249GenericsSetMap/services/PrintService.java
@@ -4,11 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PrintService {
-    private List<Integer> list = new ArrayList<>();
-    public void addValue(Integer value){
+    private List<Object> list = new ArrayList<>();
+    public void addValue(Object value){
         list.add(value);
     }
-    public Integer first(){
+    public Object first(){
         if (list.isEmpty()){
             throw new IllegalStateException("Error: List is empty");
         }
