99d8ce490b88e7e944e6197c08450c583bd5f647
jlamancio <amancio.jluis@outlook.com>
2024-02-19 05:43:10 -0300

    aula241 - Curingas Delimitados - 1ª implementação - basica

---
+++ src/main/java/cursoProgramacaoA236_A249GenericsSetMap/entities/Circle.java
@@ -0,0 +1,18 @@
+package cursoProgramacaoA236_A249GenericsSetMap.entities;
+
+public class Circle implements Shape {
+    private double radius;
+    public Circle(double radius){
+        this.radius = radius;
+    }
+    public double getRadius() {
+        return radius;
+    }
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+    @Override
+    public double area(){
+        return Math.PI * radius * radius;
+    }
+}
---
+++ src/main/java/cursoProgramacaoA236_A249GenericsSetMap/entities/Shape.java
@@ -0,0 +1,5 @@
+package cursoProgramacaoA236_A249GenericsSetMap.entities;
+
+public interface Shape {
+    double area();
+}
