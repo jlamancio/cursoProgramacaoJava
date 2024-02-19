09445ccf0b6d094cc1e793355974f4733a808d66
jlamancio <amancio.jluis@outlook.com>
2024-02-19 05:52:01 -0300

    aula241 - Curingas Delimitados - 2ª implementação - List<? extends Shape>

--- src/main/java/cursoProgramacaoA236_A249GenericsSetMap/application/mainProgram_6.java
+++ src/main/java/cursoProgramacaoA236_A249GenericsSetMap/application/mainProgram_6.java
@@ -6,17 +6,24 @@ import cursoProgramacaoA236_A249GenericsSetMap.entities.Shape;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class mainProgram_6 {
     public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
 
         List<Shape> myShapes = new ArrayList<>();
         myShapes.add(new Rectangle(3.0, 2.0));
         myShapes.add(new Circle(2.0));
 
-        System.out.println("Total area: " + totalArea(myShapes));
+        List<Circle> myCircles = new ArrayList<>();
+        myCircles.add(new Circle(2.00));
+        myCircles.add(new Circle(3.00));
+        System.out.println("Total area: " + totalArea(myCircles));
+//        System.out.println("Total area: " + totalArea(myShapes));
     }
-    public static double totalArea(List<Shape> list){
+    public static double totalArea(List<? extends Shape> list){
         double sum = 0.0;
         for (Shape s : list){
             sum += s.area();
